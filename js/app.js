8fc3c2d46232a755024aa1ca904beb2d3c42aa8e
Norbert Winklareth <norbert.winklareth@gmail.com>
2013-04-28 13:19:19 -0500

    Set gen stats data query back to default which is fetch all data.

--- js/app.js
+++ js/app.js
@@ -58,9 +58,9 @@ define([
         var gen_stats = new GenStatsView({collection: inmate_collection});
         router.on('route:gen_stats', function() {
           inmate_collection.fetch({
-            // data: { 'limit': 0 },
+            data: { 'limit': 0 },
             // data: { 'booking_date__gte': '2012-12-31', 'limit': 0 },
-            data: { 'booking_date__gte': '2013-01-01', 'limit': 0 },
+            // data: { 'booking_date__gte': '2013-01-01', 'limit': 0 },
             // data: { 'booking_date__gte': '2013-01-01', 'booking_date__lte': '2013-01-15', 'limit': 0 },
             // data: { 'booking_date__gte': '2013-03-04', 'booking_date__lte': '2013-03-10', 'limit': 0 },
             // data: { 'booking_date__gte': '2012-12-31', 'booking_date__lte': '2013-01-07', 'limit': 0 },
